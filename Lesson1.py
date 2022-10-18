2214af3b6870ad09588131c07fe75854b7bf543e
cristhom <53376765+cristhom@users.noreply.github.com>
2022-10-18 16:33:28 +0200

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -4,6 +4,9 @@ import pandas as pd
 import statistics as stat
 import matplotlib.pyplot as plt
 
+'''The per capita income of a given population is studied for which a sample of 200 people is taken,
+obtaining the following result:'''
+
 freq = 500
 min = 1000
 sup = 3000
@@ -28,17 +31,19 @@ for i,q in zip(income, quantity):
 
 print()
 print('Sample size: ', len(data))
+'''Calculate the measures of center position'''
 print('Mean: ', m)
 print('Mode: ', stat.mode(data))
 print('Median: ', stat.median(data))
 print()
+'''Calculate the measures of relative position'''
 print('Q1 quantile: ', np.quantile(data, .25))
 print('Q2 quantile: ', np.quantile(data, .50))
 print('Q3 quantile: ', np.quantile(data, .75))
 print()
+'''Is the mean representative of the study data?'''
 sd = stat.stdev(data)
 print('Standard deviaton: ', round(sd,2))
-
 if sd / m < 0.1:
     r = 'is'
 else:
